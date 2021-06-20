f0168f5e10f5b9aef41e320f35e7c1e4a3e4e2a1
GuiAquinoBR <59674194+GuiAquinoBR@users.noreply.github.com>
2021-06-20 17:47:19 -0300

    Update createTableEstados.sql

--- createTableEstados.sql
+++ createTableEstados.sql
@@ -1,5 +1,5 @@
 CREATE TABLE estados (
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
     sigla VARCHAR(2) NOT NULL,
     regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
