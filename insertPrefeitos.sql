4fe1ac93a8c8ba2fdaa1b635a3dc85e921ebeb37
GuiAquinoBR <59674194+GuiAquinoBR@users.noreply.github.com>
2021-06-20 17:53:31 -0300

    Alterando insertPrefeitos

--- insertPrefeitos.sql
+++ insertPrefeitos.sql
@@ -1,7 +1,7 @@
 INSERT INTO prefeitos
-	(nome, cidade_id)
+    (nome, cidade_id)
 VALUES
-	('Rodrigo Neves', 2),
+    ('Rodrigo Neves', 2),
     ('Raquel Lyra', 3),
     ('Zenaldo Coutinho', null)
     ('Rafael Greca', null);
