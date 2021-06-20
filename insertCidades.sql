5871fad6fe79224887a31ca96b9f563ed6ec819a
GuiAquinoBR <59674194+GuiAquinoBR@users.noreply.github.com>
2021-06-20 17:48:08 -0300

    Update insertCidades.sql

--- insertCidades.sql
+++ insertCidades.sql
@@ -1,7 +1,7 @@
 INSERT INTO cidades
-	(nome, area, estado_id)
+    (nome, area, estado_id)
 VALUES
-	('Campinas', 795, 25),
+    ('Campinas', 795, 25),
     ('Niterói', 795, 19),
     ('Caruaru', 720.6, (SELECT id FROM estados WHERE sigla = 'PE')),
     ('Juazeiro', 248.2, (SELECT id FROM estados WHERE sigla = 'CE'));
